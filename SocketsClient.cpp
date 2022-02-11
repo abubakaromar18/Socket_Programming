d92cdda0a7ef5c1c595a382790199be46384de5e
abubakaromar18 <37700067+abubakaromar18@users.noreply.github.com>
2022-02-11 15:57:58 -0600

    Create SocketsClient.cpp

---
+++ SocketsClient.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+
+using namespace std;
+
+void error(char *msg)
+{
+  perror(msg);
+  exit(0);
+}
+
+int main(int argc, char *argv)
+{
+  
+  return 0;
+}
