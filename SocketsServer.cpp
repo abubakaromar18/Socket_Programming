6b3efa71f28ccdd4dd7a33582568576daefeaf3e
abubakaromar18 <37700067+abubakaromar18@users.noreply.github.com>
2022-02-11 15:55:34 -0600

    Create SocketsServer.cpp

---
+++ SocketsServer.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+using namespace std;
+
+void error (char *msg)
+{
+  perror(msg);
+  exit(1);
+}
+
+int main(int argc, char *argv)
+{
+  
+  return 0;
+}
