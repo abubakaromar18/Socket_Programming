79e6010ca35ecc14f790f0e7892900e911f11b14
abubakaromar18 <37700067+abubakaromar18@users.noreply.github.com>
2022-02-11 19:57:27 -0600

    Updated for multiple messages

--- SocketsClient.cpp
+++ SocketsClient.cpp
@@ -18,24 +18,31 @@ void error(char *msg)
 
 int main(int argc, char **argv)
 {
+  //DEFINING VARIABLES
   int sockfd, portno, n;
   struct sockaddr_in serv_addr;
   struct hostent *server;
-
   char buffer[256];
+  bool close_connection = false;
+
+  //CHECKS THE ARGUMENT COUNT TO MAKE SURE THE HOSTNAME AND PORT NUMBER IS ENTERED
   if(argc < 3)
   {
     fprintf(stderr, "usage %s hostname port\n", argv[0]);
     exit(0);
   }
 
+  //CONVERTS THE PORT FROM STRING TO INTEGER
   portno = atoi(argv[2]);
+
+  //CREATES A SOCKET FILE DESCRIPTOR
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if(sockfd < 0)
   {
     error("ERROR opening socket");
   }
 
+  //FINDS HOST USING THE HOSTNAME IN THE ARGUMENT
   server = gethostbyname(argv[1]);
   if(server == NULL)
   {
@@ -44,34 +51,47 @@ int main(int argc, char **argv)
   }
 
   bzero((char *) &serv_addr, sizeof(serv_addr));
+
+  //DEFINING THE SERVER ADDRESS
   serv_addr.sin_family = AF_INET;
   bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
   serv_addr.sin_port = htons(portno);
+
+  //CONNECTING THE CLIENT TO THE SERVER
   if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
   {
     error("ERROR connecting");
   }
 
-  printf("Please enter the message: ");
-  bzero(buffer, 256);
-  fgets(buffer, 255, stdin);
+  while(!close_connection)
+  {
+    printf("Please enter the message: ");
+    bzero(buffer, 256);
+    fgets(buffer, 255, stdin);
   
-  n = write(sockfd, buffer, strlen(buffer));
-  if(n < 0)
-  {
-    error("ERROR writing to socket");
+    n = write(sockfd, buffer, strlen(buffer));
+    if(n < 0)
+    {
+      error("ERROR writing to socket");
+    }
+
+    bzero(buffer, 256);
+    n = read(sockfd, buffer, 255);
+    if(n < 0)
+    {
+      error("ERROR reading from socket");
+    }
+
+    if(strcmp(buffer, "exit") == 0)
+    {
+      close_connection = true;
+      printf("Connection Terminated");
+    }
+    else
+    {
+      printf("%s\n", buffer);
+    }
   }
-
-  bzero(buffer, 256);
-  n = read(sockfd, buffer, 255);
-  if(n < 0)
-  {
-    error("ERROR reading from socket");
-  }
-
-  printf("%s\n", buffer);
-  return 0;
-
 
   return 0;
 }
--- SocketsServer.cpp
+++ SocketsServer.cpp
@@ -16,18 +16,21 @@ void error (char *msg)
 
 int main(int argc, char **argv)
 {
+  //DEFINING VARIABLES
+  struct sockaddr_in serv_addr, cli_addr;
   int sockfd, newsockfd, portno, clilen;
   char buffer[256];
   int n;
+  bool close_connection = false;
 
-  struct sockaddr_in serv_addr, cli_addr;
-
+  //CHECKS THE ARGUMENT COUNT TO MAKE SURE THE PORT NUMBER IS ENTERED
   if (argc < 2)
   {
     fprintf(stderr, "ERROR, no port provided\n");
     exit(1);
   }
 
+  //CREATES A SOCKET FILE DESCRIPTOR
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if(sockfd < 0)
   {
@@ -35,41 +38,63 @@ int main(int argc, char **argv)
   }
 
   bzero((char *) &serv_addr, sizeof(serv_addr));
+
+  //CONVERTS THE PORT FROM STRING TO INTEGER
   portno = atoi(argv[1]);
 
+  //DEFINING THE SERVER ADDRESS
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(portno);
   serv_addr.sin_addr.s_addr = INADDR_ANY;
 
+
+  //BINDING THE SOCKET TO THE SPECIFIED IP AND PORT
   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
   {
     error("ERROR on binding");
   }
 
-  listen(sockfd, 5);
+  //LISTENS TO THE NETWORK FOR ANY CONNECTIONS
+  listen(sockfd, 5); //ALLOWS AT MOST 5 CONNECTIONS
+
   clilen = sizeof(cli_addr);
 
+  //ACCEPTS CLIENT SOCKET
   newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *) &clilen);
   if(newsockfd < 0)
   {
     error("ERROR on accept");
   }
 
-  bzero(buffer, 256);
-  n = read(newsockfd, buffer, 255);
-  if(n < 0)
+  while(!close_connection)
   {
-    error("ERROR reading from socket");
+    bzero(buffer, 256);
+    n = read(newsockfd, buffer, 255);
+    if(n < 0)
+    {
+      error("ERROR reading from socket");
+    }
+
+    if(strcmp(buffer, "exit") == 0)
+    {
+      close_connection = true;
+      n = write(newsockfd, "exit", 4);
+      if(n < 0)
+      {
+        error("ERROR writing to socket");
+      }
+    }
+    else
+    {
+      printf("Here is the message: %s\n", buffer);
+
+      n = write(newsockfd, "I got your message", 18);
+      if(n < 0)
+      {
+        error("ERROR writing to socket");
+      }
+    }
   }
-
-  printf("Here is the message: %s\n", buffer);
-
-  n = write(newsockfd, "I got your message", 18);
-  if(n < 0)
-  {
-    error("ERROR writing to socket");
-  }
-
 
   return 0;
 }
